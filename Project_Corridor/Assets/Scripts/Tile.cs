3744bf60326754436d213dd9b62d6c17a05214d5
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-11-02 19:26:11 -0400

    Call to procedural generation engine
    - Added class to handle translating tile gameobjects to engine input text file
    - Added class to handle calls to CLIPS procedural generation engine
    - Moved external dependencies to root project folder

---
+++ Project_Corridor/Assets/Scripts/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapTiling
+{
+    public enum CardinalDirection
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    public enum TileType
+    {
+        START,
+        GOAL,
+        ROOM,
+        SPECIAL
+    }
+
+    public class Tile : MonoBehaviour
+    {
+        public TileType type;
+        public Vector2 GridSize;
+        public bool NorthEdge;
+        public bool SouthEdge;
+        public bool EastEdge;
+        public bool WestEdge;
+        public Dictionary<CardinalDirection, bool> EdgeOpenings = new Dictionary<CardinalDirection, bool>();
+
+        private Dictionary<CardinalDirection, Tile> EdgeAdjacency;
+        private Vector2 coordinates;
+        private int rotations;
+        private bool assembled;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            EdgeOpenings[CardinalDirection.NORTH] = NorthEdge;
+            EdgeOpenings[CardinalDirection.SOUTH] = SouthEdge;
+            EdgeOpenings[CardinalDirection.EAST] = EastEdge;
+            EdgeOpenings[CardinalDirection.WEST] = WestEdge;
+        }
+        
+    }
+}
---
+++ Project_Corridor/Assets/Scripts/TileAssembler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace MapTiling {
+
+    public class TileAssembler : MonoBehaviour
+    {
+        public string assemblerLocation;
+        public string engineLocation;
+        public string mapGenFolder;
+        public TileInputWriter writer;
+
+        private string fullAssemblePath;
+        private string mapSrcLocation;
+        private string mapDstLocation;
+        private string engineFullPath;
+
+        const string srcName = "mapSrc.txt";
+        const string dstName = "mapDst.csv";
+
+        private void Start()
+        {
+            fullAssemblePath = Directory.GetCurrentDirectory() + "\\" + assemblerLocation;
+            mapSrcLocation = Directory.GetCurrentDirectory() + "\\" + mapGenFolder + "\\" + srcName;
+            mapDstLocation = Directory.GetCurrentDirectory() + "\\" + mapGenFolder + "\\" + dstName;
+            engineFullPath = Directory.GetCurrentDirectory() + "\\" + engineLocation;
+
+            writer.WriteToFile(mapSrcLocation);
+            RunEngineExternal();
+        }
+
+        void RunEngineExternal()
+        {
+            try
+            {
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+
+                proc.StartInfo.FileName = fullAssemblePath;
+                proc.StartInfo.Arguments = mapSrcLocation + " " + mapDstLocation + " " + engineFullPath;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+
+                proc.OutputDataReceived += OutputHandler;
+                proc.ErrorDataReceived += OutputHandler;
+                proc.EnableRaisingEvents = true;
+
+                proc.Start();
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log("Encountered an error!");
+                Debug.Log(e.ToString());
+            }
+
+        }
+
+        private static void OutputHandler(object sendingProcess,
+                System.Diagnostics.DataReceivedEventArgs outLine)
+        {
+            Debug.Log(outLine.Data);
+        }
+    }
+}
