d11af97e9bfb985f593fc33baf95005881f8714f
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-11-05 17:42:42 -0500

    CLIPS I/O Integration with Unity (#5)
    * Call to procedural generation engine
    
    - Added class to handle translating tile gameobjects to engine input text file
    - Added class to handle calls to CLIPS procedural generation engine
    - Moved external dependencies to root project folder
    
    * Read from procedural generation engine
    
    Added functionality to read back CSV data to inform Tile object properties after procedural generation engine runs
    
    * Tile Reader Coroutine
    
    Spun off reading operation into coroutine that yields back while the external AssembleNET application runs

---
+++ Project_Corridor/Assets/Scripts/TileOutputReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace MapTiling {
+    public class TileOutputReader : MonoBehaviour
+    {
+        const int NUM_FIELDS = 9;
+
+        public void ReadFromFile(string file, List<Tile> tiles)
+        {
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string line = null;
+                Tile match = null, adj = null;
+                string id = null;
+                while(!reader.EndOfStream)
+                {
+                    // Sample line:
+                    // tile,backfill-N-gen2,0,2,0,nil,Tile4,nil,nil
+                    if ((line = reader.ReadLine()).StartsWith("tile"))
+                    {
+                        String[] fields = line.Split(',');
+                        id = fields[1];
+                        Debug.Log("Name of tile is " + id);
+                        Console.WriteLine("Name of tile is " + id);
+                        match = tiles.Find(t => t.name.Equals(id));
+                        if(match != null && fields.Length == NUM_FIELDS)
+                        {
+                            match.coordinates.x = int.Parse(fields[2]);
+                            match.coordinates.y = int.Parse(fields[3]);
+                            match.rotations = int.Parse(fields[4]);
+
+                            foreach(CardinalDirection c in Enum.GetValues(typeof(CardinalDirection)))
+                            {
+
+                                adj = (fields[5 + (int)c].Equals("nil")) ? null : tiles.Find(t => t.name.Equals(fields[5 + (int)c]));
+                                match.EdgeAdjacency[c] = adj;
+                            }
+
+                            match.assembled = true;
+
+                        } else if(id.Contains("gen"))
+                        {
+                            //TODO: generate new tile
+                            continue;
+                        }
+                    }
+                }
+            }
+            
+        }
+
+
+    }
+}
