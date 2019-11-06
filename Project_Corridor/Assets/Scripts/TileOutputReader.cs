97ac675140a7f0e8159f1702000d3880163b9097
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-11-06 01:31:44 -0500

    Backfill and Big
    - Added instantiations for 'backfill' tiles generated during testing to fill gaps
    - Added support for placing tiles larger than 1x1

--- Project_Corridor/Assets/Scripts/TileOutputReader.cs
+++ Project_Corridor/Assets/Scripts/TileOutputReader.cs
@@ -7,7 +7,10 @@ using UnityEngine;
 namespace MapTiling {
     public class TileOutputReader : MonoBehaviour
     {
-        const int NUM_FIELDS = 9;
+        const int NUM_TILE_FIELDS = 9;
+        const int NUM_MULTI_FIELDS = 6;
+
+        public Tile BackfillPrefab;
 
         public void ReadFromFile(string file, List<Tile> tiles)
         {
@@ -26,26 +29,65 @@ namespace MapTiling {
                         id = fields[1];
                         Debug.Log("Name of tile is " + id);
                         Console.WriteLine("Name of tile is " + id);
-                        match = tiles.Find(t => t.name.Equals(id));
-                        if(match != null && fields.Length == NUM_FIELDS)
+                        if (!id.Contains("backfill"))
+                        {
+                            match = tiles.Find(t => t.name.Equals(id));
+                            if(match != null)
+                                match.rotations = int.Parse(fields[4]);
+
+                        } else
+                        {
+                            match = Instantiate(BackfillPrefab);
+                            match.name = id;
+                            tiles.Add(match);
+
+                            if(id.Contains("backfill-N"))
+                            {
+                                match.rotations = 2;
+                            } else if(id.Contains("backfill-E"))
+                            {
+                                match.rotations = 3;
+                            } else if(id.Contains("backfill-S"))
+                            {
+                                match.rotations = 0;
+                            } else if(id.Contains("backfill-W"))
+                            {
+                                match.rotations = 1;
+                            } else
+                            {
+                                match.rotations = 0;
+                            }
+                        }
+                        if (match != null && fields.Length == NUM_TILE_FIELDS)
                         {
                             match.coordinates.x = int.Parse(fields[2]);
                             match.coordinates.y = int.Parse(fields[3]);
-                            match.rotations = int.Parse(fields[4]);
 
-                            foreach(CardinalDirection c in Enum.GetValues(typeof(CardinalDirection)))
+                            foreach (CardinalDirection c in Enum.GetValues(typeof(CardinalDirection)))
                             {
-
                                 adj = (fields[5 + (int)c].Equals("nil")) ? null : tiles.Find(t => t.name.Equals(fields[5 + (int)c]));
                                 match.EdgeAdjacency[c] = adj;
                             }
 
                             match.assembled = true;
-
-                        } else if(id.Contains("gen"))
+                        }
+                        else if (id.Contains("backfill"))
                         {
+                            
                             //TODO: generate new tile
                             continue;
+                        }
+                    }
+                    else if (line.StartsWith("multitile"))
+                    {
+                        String[] fields = line.Split(',');
+                        id = fields[1];
+                        match = tiles.Find(t => t.name.Equals(id));
+                        if (match != null && fields.Length == NUM_MULTI_FIELDS)
+                        {
+                            match.coordinates.x = (float)(int.Parse(fields[2]) + int.Parse(fields[3])) / 2;
+                            match.coordinates.y = (float)(int.Parse(fields[4]) + int.Parse(fields[5])) / 2;
+                            match.assembled = true;
                         }
                     }
                 }
