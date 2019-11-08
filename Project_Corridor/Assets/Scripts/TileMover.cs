e559814096bc183ceccff92ae9673f298dcbf14b
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-11-08 14:54:15 -0500

    Code cleanups
    - Added comments and more descriptive labels to MapTiling files
    - Standardized events and added event for when map generation is completed

--- Project_Corridor/Assets/Scripts/TileMover.cs
+++ Project_Corridor/Assets/Scripts/TileMover.cs
@@ -3,22 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace MapTiling {
+    // Position Tiles in the scene based on their class properties
     public class TileMover : MonoBehaviour
     {
-        public Vector2 sideLength;
-        public Vector3 Offset;
+        #region Public Members
+        // Translation of x/z side length from grid to transform; default is 1 grid unit : 10 meters
+        public Vector2 sideLength = new Vector2(10, 10);
 
+        // Transform offset for grid origin; default is 0
+        public Vector3 Offset = new Vector3(0, 0, 0);
+        #endregion
+
+        /* Translate Tile instance to scene coordinates based on its x/z/r properties
+         * Arguments:   tiles:  List of tiles to translate
+         */
         public void MoveTile(List<Tile> tiles)
         {
             foreach(Tile t in tiles)
             {
-                t.transform.position = new Vector3(t.coordinates.x * sideLength.x + Offset.x,
-                                                    Offset.y,
-                                                    t.coordinates.y * sideLength.y + Offset.y);
-                t.transform.Rotate(0, 90 * t.rotations, 0);
+                if (t.assembled)
+                {
+                    t.transform.position = new Vector3(t.coordinates.x * sideLength.x + Offset.x,
+                                                        Offset.y,
+                                                        t.coordinates.y * sideLength.y + Offset.y);
+                    t.transform.Rotate(0, 90 * t.rotations, 0);
+                } else
+                {
+                    // If a tile was not assembled remove it from the scene
+                    Destroy(t);
+                }
             }
         }
 
+        /* Move an object on top of a specified tile. Typically used to recenter the Player on START.
+         * Arguments:   obj:    Object to move
+         *              t:      Destination tile
+         */
         public void MoveToTile(GameObject obj, Tile t)
         {
             Vector3 destPos = t.transform.position;
