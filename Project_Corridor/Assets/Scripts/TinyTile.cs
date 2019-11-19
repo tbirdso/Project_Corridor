9952e82d3736334b897482ed8aec67d773ab3b31
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-11-19 17:22:46 -0500

    Tiles and Teleports (#12)
    - Basic TinyTiles
    - Teleport at goal loads new scene

---
+++ Project_Corridor/Assets/Scripts/TinyTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapTiling {
+    public class TinyTile : MonoBehaviour
+    {
+        private Tile ParentTile;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Tile pTile = GetComponentInParent<Tile>();
+            ParentTile = (pTile == null) ? null : pTile;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
