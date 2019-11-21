acb1a82789e4768b0e70865d8b8acfcb8ae9edf3
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-11-21 17:19:44 -0500

    Inserted code for TeleportPoint scene transition
    Added hook to Valve.VR.InteractionSystem.TeleportPoint script for scene reloading on finish

---
+++ Project_Corridor/Assets/Scripts/Test_TeleportPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Test_TeleportPoint : MonoBehaviour
+{
+    public bool test = false;
+    public TeleportPoint tp;
+
+    public void Update()
+    {
+        if(test)
+        {
+            tp.TeleportToScene();
+            test = false;
+        }
+    }
+}
