6de089706ce7edaf54baeabd072d02fe57a7fa27
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-12-04 00:13:42 -0500

    ProBuilder Map Tiling
    Updated map tiling to use ProBuilder prefabs

---
+++ Project_Corridor/Assets/Scripts/SceneCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneCollision : MonoBehaviour
+{
+    public string sceneToTransition;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "HeadCollider")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToTransition);
+        }
+        else
+        {
+            Debug.Log("Collided with " + collision.gameObject.name);
+        }
+    }
+}
